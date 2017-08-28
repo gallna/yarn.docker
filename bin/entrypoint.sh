626009fa18836151896f6e724070fb817aa2d64d
Tomasz Jonik <tom.jonik@hotmail.com>
2017-08-28 01:59:25 +0100

    Update to work with Kubernetes

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,25 +5,44 @@
 # Exit on error inside any functions or subshells.
 # set -o errtrace
 
-print_env() {
-  printenv | sort
-  echo && IFS=:; printf "%s\n" $PATH && echo
-  echo && IFS=:; printf "%s\n" $CLASSPATH && echo
-  echo && IFS=:; printf "%s\n" $(hdfs classpath) && echo
+classpath() { find $1 -type f -name "*.jar" | paste -sd:; }
+
+# Valid log levels: ALL, TRACE, DEBUG, [INFO], WARN, ERROR, FATAL, OFF
+set_log_level() {
+  test -f "${HADOOP_CONF_DIR}/log4j.properties" && \
+    sed -i "s~=\(ALL\|TRACE\|DEBUG\|INFO\|WARN\|ERROR\)~=${1-LOG_LEVEL}~g" "${HADOOP_CONF_DIR}/log4j.properties"
+  test -f "${SPARK_HOME-}/conf/log4j.properties" && \
+    sed -i "s~=\(ALL\|TRACE\|DEBUG\|INFO\|WARN\|ERROR\)~=${1-LOG_LEVEL}~g" "${SPARK_HOME}/conf/log4j.properties"
 }
 
-classpath() {
-  # find $1 -type f -name "*.jar" | xargs -I{} dirname {} | uniq | sed -e 's/$/\/\*/' | paste -sd:
-  find $1 -type f -name "*.jar" | paste -sd:
+update_config() {
+  if [[ -n "$1" && -f "$2" ]]; then
+    echo "${1}" | UPDATE=1 $(dirname $0)/config.sh "$2"
+    echo >&2 $(basename "$2") updated
+  fi
 }
+
+mkdir -p /var/log/logs
+test -z "${LOG_LEVEL-}" || set_log_level ${LOG_LEVEL-}
+
+update_config "$core_site" "${HADOOP_CONF_DIR}/core-site.xml"
+update_config "$hdfs_site" "${HADOOP_CONF_DIR}/hdfs-site.xml"
+update_config "$yarn_site" "${YARN_CONF_DIR}/yarn-site.xml"
+update_config "$mapred_site" "${HADOOP_CONF_DIR}/mapred-site.xml"
+update_config "$capacity_scheduler" "${HADOOP_CONF_DIR}/capacity-scheduler.xml"
 
 export CLASSPATH=${CLASSPATH-$(classpath $HADOOP_HOME)}
 export SPARK_CLASSPATH=${SPARK_CLASSPATH-$(classpath $SPARK_HOME)}
 export JAVA_LIBRARY_PATH=${JAVA_LIBRARY_PATH-"$HADOOP_HOME/lib/native"}
 export HDFS_CLUSTER_NAME=${HDFS_CLUSTER_NAME-"the-cluster"}
+export STDOUT_LOGS_TARGET=${STDOUT_LOGS_TARGET-/var/log/logs}
 export SPARK_NO_DAEMONIZE=true
+export SPARK_LOG_DIR=/var/log/logs
+export HADOOP_LOG_DIR=/var/log/logs
 
 source $HADOOP_PREFIX/libexec/hadoop-config.sh
+source $SPARK_HOME/sbin/spark-config.sh
+source $SPARK_HOME/bin/load-spark-env.sh
 
 spark-submit() {
   $SPARK_HOME/bin/spark-submit \
@@ -31,7 +50,7 @@ spark-submit() {
   	--master yarn \
     --queue default \
   	--deploy-mode client \
-    /usr/local/spark/examples/jars/spark-examples_2.11-2.2.0.jar
+    $SPARK_HOME/examples/jars/spark-examples_2.11-2.2.0.jar
 }
 
 ### NameNode ### $HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode ###
@@ -52,7 +71,6 @@ datanode() {
 sparkmaster() {
   # Run a script to start Spark Master:
   $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
-  stdout-logs ${SPARK_HOME}/logs
 }
 
 ### SparkWorker ### $SPARK_HOME/sbin/start-slave.sh spark://spark-master:7077 ###
@@ -60,15 +78,16 @@ sparkworker() {
   # Run a script to start Spark Worker:
   $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker \
     spark://${SPARK_MASTER_HOST-spark-master}:${SPARK_MASTER_PORT-7077}
-  stdout-logs ${SPARK_HOME}/logs
 }
 
-### Spark JobHistory Server ### $SPARK_HOME/bin/spark-class org.apache.spark.deploy.history.HistoryServer 1 ###
+### Spark JobHistory Server ### $SPARK_HOME/sbin/start-history-server.sh ###
 spark-history() {
   hdfs dfs -mkdir -p /shared/spark-logs
+  hdfs dfs -chmod -R 1777 /shared/spark-logs
+  hdfs dfs -chown nobody:nobody /shared/spark-logs
+  hdfs dfs -ls -R /shared
   # Start the MapReduce Spark Server with the following command, run on the designated server:
-  $SPARK_HOME/sbin/start-history-server.sh
-  stdout-logs ${SPARK_HOME}/logs
+  $SPARK_HOME/bin/spark-class org.apache.spark.deploy.history.HistoryServer
 }
 
 ### MapReduce JobHistory Server ### $HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver ###
@@ -100,35 +119,6 @@ web-app-proxy() {
 # Typically you choose one machine in the cluster to act as the NameNode and one machine as to act as the ResourceManager, exclusively.
 # The rest of the machines act as both a DataNode and NodeManager and are referred to as slaves.
 
-# Valid log levels: ALL, TRACE, DEBUG, [INFO], WARN, ERROR, FATAL, OFF
-log-level() {
-  test -f $HADOOP_CONF_DIR/log4j.properties && \
-    sed "s~=\(ALL\|TRACE\|DEBUG\|INFO\|WARN\|ERROR\),\?~=${LOG_LEVEL}~g" $HADOOP_CONF_DIR/log4j.properties
-  test -f ${SPARK_HOME-}/conf/log4j.properties && \
-    sed "s~=\(ALL\|TRACE\|DEBUG\|INFO\|WARN\|ERROR\),\?~=${LOG_LEVEL}~g" $SPARK_HOME/conf/log4j.properties
-}
-
-update_config() {
-  if [[ -n "$1" && -f "$2" ]]; then
-    echo "${1}" | UPDATE=1 $(dirname $0)/config.sh "$2"
-    echo >&2 $(basename "$2") updated
-  fi
-}
-test -n ${LOG_LEVEL-} && log-level
-update_config "$core_site" "${HADOOP_CONF_DIR}/core-site.xml"
-update_config "$hdfs_site" "${HADOOP_CONF_DIR}/hdfs-site.xml"
-update_config "$yarn_site" "${YARN_CONF_DIR}/yarn-site.xml"
-update_config "$mapred_site" "${HADOOP_CONF_DIR}/mapred-site.xml"
-update_config "$capacity_scheduler" "${HADOOP_CONF_DIR}/capacity-scheduler.xml"
-
-# keep container running by displaying selected service logs
-stdout-logs() {
-  local logs=${1-${STDOUT_LOGS_TARGET-${HADOOP_PREFIX}/logs}}
-  test -e ${logs} && while true; do
-    if [[ -d ${logs} ]]; then tail -f ${logs}/*; else tail -f ${logs}; fi
-  done
-}
-
 help() {
   cat <<HELPMESSAGE
   Usage: `basename $0` [-h] [-c] [-y] [-e] [-n] [-N] [-d] [-D] [-m] [-r] [-w] [-j]
@@ -158,13 +148,31 @@ HELPMESSAGE
 # start bash if nothing selected
 [[ 0 == $# ]] && echo "missing command" && exit 2
 
+# keep container running by displaying selected service logs
+stdout-logs() {
+  local logs=${1-${STDOUT_LOGS_TARGET}}; let counter=1
+  while true; do
+    if [[ -d ${logs} ]]; then
+      [[ $(ls -1q ${logs} | wc -l) -gt 0 ]] && tail --retry -f ${logs}
+    else
+      [[ -f ${logs} ]] && tail -f ${logs}
+    fi
+    if (( (counter++ % 15) == 0 )); then
+      [[ -e ${logs} ]] || echo >&2 $(date +"%x %H:%M:%S WARN") "Unexpected logs target: ${logs}"
+      echo >&2 $(date +"%x %H:%M:%S DEBUG") "Waiting for logs in ${logs} [log-level: ${LOG_LEVEL-}]"
+    fi
+    sleep 2
+  done
+}
+
 exec 3>&1
 exec 4>&2
 
 with_logger() {
-  exec 1> >(while read line; do echo "$(basename $1 .sh): $line"; done >&3)
-  exec 2> >(while read line; do echo "$(basename $1 .sh): $line"; done >&4)
-  type $1 | grep 'function' > /dev/null && eval "$@" || exec $@
+  label=$1; shift
+  exec 1> >(while read line; do echo "$label: $line"; done >&3)
+  exec 2> >(while read line; do echo "$label: $line"; done >&4)
+  eval "$@"
 }
 
 OPTIND=1
@@ -177,8 +185,8 @@ while getopts "hswWnNdDmrwjJp-:" opt; do
     s) sparkmaster ;;
     w) sparkworker ;;
     N) namenode ;;
-    D) with_logger node-manager & datanode ;;
-    W) with_logger node-manager & sparkworker ;;
+    D) with_logger 'nm' node-manager & datanode ;;
+    W) with_logger 'nm' node-manager & sparkworker ;;
     m) node-manager ;;
     r) resource-manager ;;
     p) web-app-proxy ;;
@@ -213,6 +221,7 @@ if [[ $# -gt 0 ]]; then
 fi
 
 # Use STDOUT_LOGS_TARGET environment to display particular log file/directory
-STDOUT_LOGS_TARGET=${STDOUT_LOGS_TARGET-${HADOOP_PREFIX}/logs}
+# STDOUT_LOGS_TARGET=${STDOUT_LOGS_TARGET-${HADOOP_PREFIX}/logs}
 # keep container running by displaying selected service logs
-stdout-logs ${STDOUT_LOGS_TARGET}
+# stdout-logs ${STDOUT_LOGS_TARGET}
+# wait
